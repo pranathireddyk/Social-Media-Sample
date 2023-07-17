03f3891611b5b661318f5fb44d725268c34e74c6
Pranathi Kunadi <pranathikunadi@Pranathis-MacBook-Air.local>
2023-07-17 00:06:07 -0400

    today's work

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,22 @@
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
 import postRoutes from './routes/posts.js';
 
 const app = express();
-app.use('/posts', postRoutes);
 
-app.use(bodyParser.json({limit: "30mb", extended:true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://<pranathireddyk>:<Pr@nath1>@cluster1.hfll3b8.mongodb.net/?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5000;
+app.use('/posts', postRoutes);
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => app.listen(PORT, () => console.log('server runing on port: ${PORT}')))
-.catch((error)=> console.log(error.message));
+const CONNECTION_URL = 'mongodb+srv://pranathireddyk:Pranath1@cluster1.hfll3b8.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5020;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
