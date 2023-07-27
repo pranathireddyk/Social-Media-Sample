3c26d14e1b3042a3ec4d52d0e13e2142aefb0fa6
Pranathi Kunadi <pranathikunadi@Pranathis-MacBook-Air.local>
2023-07-27 00:21:46 -0400

    Final code

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/user.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://pranathireddyk:Pranath1@cluster1.hfll3b8.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5020;
